61f024356185a90e65108eeaada36c48c7ec7362
Chase <chase.lindsay1@gmail.com>
2022-02-28 18:23:59 -0600

    navbar and favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,14 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
 //= require rails-ujs
+//= require jquery
 //= require activestorage
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(document).on('turbolinks:load', function() {
+	$('.ui.dropdown').dropdown();
+});
